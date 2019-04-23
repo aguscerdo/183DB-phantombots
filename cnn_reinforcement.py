831f2c62b44ec8ee36b31ab94d67654bd49e7246
aguscerdo <agus.marinovic@gmail.com>
2019-04-22 17:48:17 -0700

    model builds

--- cnn_reinforcement.py
+++ cnn_reinforcement.py
@@ -3,10 +3,10 @@ import numpy as np
 import keras as k
 
 class MultiAgentCNN:
-	def __init__(self, params):
+	def __init__(self, params=None):
 		self.params = dict()
 		self.set_default_params()
-	
+		self.model = k.Model()
 	
 	def set_default_params(self):
 		self.params = {
@@ -17,7 +17,10 @@ class MultiAgentCNN:
 			'cnn4': (3, 3, 32),
 			'cnn5': (3, 3, 32),
 			'cnn6': (3, 3, 32),
-			'output': 2
+			'dropout': 0.3,
+			'nn1': 128,
+			'nn2': 64,
+			'output': 4
 		}
 	
 	
@@ -27,11 +30,44 @@ class MultiAgentCNN:
 		b1 = k.layers.BatchNormalization()(c1)
 		c2 = k.layers.Conv2D(layer2[2], (layer2[0], layer2[1]), padding='SAME',)(b1)
 		
-		concat = k.layers.Concatenate([layer_in, c2])
+		concat = k.layers.Concatenate()([layer_in, c2])
 		out = k.layers.MaxPooling2D()(concat)
 		
 		return out
 	
 	
-	
+	def fully_connected(self, layer_in):
+		nn = k.layers.Dense(self.params['nn1'], activation='relu')(layer_in)
+		nn = k.layers.BatchNormalization()(nn)
+		nn = k.layers.Dropout(self.params['dropout'])(nn)
+		nn = k.layers.Dense(self.params['nn2'], activation='relu')(nn)
+		nn = k.layers.BatchNormalization()(nn)
+		nn = k.layers.Dropout(self.params['dropout'])(nn)
 		
+		return k.layers.Dense(self.params['output'], activation='softmax')(nn)
+
+	
+	def build(self):
+		layer_in = k.layers.Input((10, 10, 4), (None, 10, 10, 4), sparse=False)
+		res = self.residual_layer(layer_in, self.params['cnn1'], self.params['cnn2'])
+		res = self.residual_layer(res, self.params['cnn3'], self.params['cnn4'])
+		
+		flat = k.layers.Flatten()(res)
+		out = self.fully_connected(flat)
+		
+		self.model = k.Model(inputs=layer_in, outputs=out)
+		
+		
+	def compile(self):
+		if self.model is None:
+			return
+		
+		self.model.compile('ADAM')
+	
+	
+
+if __name__ == '__main__':
+	m = MultiAgentCNN()
+	m.build()
+	# m.compile()
+	m.model.summary()
